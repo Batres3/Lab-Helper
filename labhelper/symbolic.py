cd1752400846e99c615ad66e49c7d278d9ddf908
Batres <wimbledonproject@hotmail.com>
2023-10-06 00:02:23 +0200

    Data storage in Helper class and start of units

--- labhelper/symbolic.py
+++ labhelper/symbolic.py
@@ -1,7 +1,7 @@
-import sympy as sp
+from sympy import (Symbol, Mul, Add, Pow, diff, sqrt, parse_expr, solve, Equality, init_printing)
 from IPython.display import display
 
-sp.init_printing()
+init_printing()
 
 def list_swap(l: list, x, y):
     if x not in l or y not in l:
@@ -20,51 +20,48 @@ class Helper:
     """
     # For Readers looking at the documentation from source code keep in mind \\ is equivalent to \ because of the manner in which python parses docstrings
     def __init__(self, func_str: str, vars_in: list[str] , const_in: list[str] = [], error_mark: str = r"\Delta ") -> None:
-        self.vars_text = vars_in
-        self.vars = [sp.Symbol(e) for e in vars_in]
-        self.consts_text = const_in
+        self._vars_text = vars_in
+        self._vars: list[Symbol] = [Symbol(e) for e in vars_in]
+        self._consts_text = const_in
         if const_in:
-            self.consts = [sp.Symbol(e) for e in const_in]
+            self._consts: list[Symbol] = [Symbol(e) for e in const_in]
         else:
-            self.consts = []
-        all_vars = self.vars + self.consts
-        text = vars_in + const_in
+            self._consts: list[Symbol] = []
 
+        self._error_mark: str = error_mark
+        self._errors_text: list[str] = [self._error_mark + a for a in vars_in]
+        self._error_function = 0
+        self._errors: list[Symbol] = [Symbol(a) for a in self._errors_text]
+
+        all_vars: list[Symbol] = self._vars + self._consts
+        text: list[str] = vars_in + const_in
         [list_swap(text, x, y) for x in text for y in text if x in y and text.index(x) < text.index(y)]
-        uni = [str(chr(0x0F0 + i)) for i in range(len(text))]
+        uni: list[str] = [str(chr(0x0F0 + i)) for i in range(len(text))]
         for i in range(len(text)):
             func_str = func_str.replace(text[i], uni[i])
         dict_in = {uni[i]: all_vars[i] for i in range(len(text))}
 
-        self.function = sp.parse_expr(func_str, dict_in)
+        self._function = parse_expr(func_str, dict_in)
+        self.data_errors = {e: None for e in self._errors_text}
+        self.data_variables = {e: None for e in text}
+        self.calculate_error_function()
         
-        if not all(item in self.function.atoms(sp.Symbol) for item in self.vars + self.consts):
-            raise ValueError("The given variables and/or constants are not the same as in the given function")
-
-        self.error_mark = error_mark
-        self.errors_text = [self.error_mark + a for a in vars_in]
-        self.errors = [sp.Symbol(a) for a in self.errors_text]
-        self.error_function = None
+        if not all(item in self._function.atoms(Symbol) for item in self._vars + self._consts) or len(self._function.atoms(Symbol)) != len(self._vars + self._consts):
+            raise ValueError("The given variables and/or constants are not the same as in the given _function")
     
-    def function_input_check(self):
-        """
-        Shows all inputs to the function
-        """
-        display(self.function.atoms(sp.Symbol))
-
     def display_data(self):
         """
         Prints the function, constants and variables
         """
         print("Variables:")
-        display(self.vars)
+        display(self._vars)
         print("Constants:")
-        display(self.consts)
+        display(self._consts)
         print("Function:")
-        display(self.function)
-        if self.error_function != None:
+        display(self._function)
+        if self._error_function != None:
             print("Error Function:")
-            display(self.error_function)
+            display(self._error_function)
     
     def calculate_error_function(self):
         """
@@ -72,14 +69,15 @@ class Helper:
 
         Delta f(x1,...,xn) = sqrt((Delta x1 * df/dx1)^2 + ... + (Delta xn * df/dxn)^2)
         """
-        self.error_function = 0
-        for i in range(len(self.vars)): 
-            a = sp.Mul(self.errors[i], sp.diff(self.function, self.vars[i]), evaluate= False)
-            b = sp.Pow(a, 2, evaluate=False)
-            self.error_function = sp.Add(self.error_function, b)
-        self.error_function = sp.sqrt(self.error_function)
+        if self._error_function:
+            return self._error_function
+        for i in range(len(self._vars)): 
+            a = Mul(self._errors[i], diff(self._function, self._vars[i]), evaluate= False)
+            b = Pow(a, 2, evaluate=False)
+            self._error_function = Add(self._error_function, b)
+        self._error_function = sqrt(self._error_function)
         
-    def evaluate_function(self, subs: dict, as_float: bool = False):
+    def evaluate_function(self, subs: dict = {}):
         """
         Evaluates the function with the given values, which must be provided in the form of a Dictionary, for instance:
 
@@ -91,11 +89,35 @@ class Helper:
 
         evaluate_function({"A": 0.5, "C": 3}) This will leave "b" and "d" as variables
         """
-        if not all(item in self.consts_text + self.vars_text for item in subs.keys()):
-            raise ValueError("The given variables and/or constants are not the same as in the given function")
-        return float(self.function.evalf(subs=subs)) if as_float else self.function.evalf(subs=subs)
+        if subs:
+            if not all(item in self._consts_text + self._vars_text for item in subs.keys()):
+                raise ValueError("The given variables and/or constants are not the same as in the given function")
+            return float(self._function.evalf(subs=subs))
+        
+        subs = {e[0]: e[1] for e in self.data_variables.items() if e[1] != None}
+        if any(isinstance(e, list) for e in subs.values()):
+            list_keys = [e[0] for e in list(subs.items()) if isinstance(e[1], list)]
+            n = len(subs[list_keys[0]])
+            if not all(n == e for e in [len(subs[e]) for e in list_keys]):
+                raise ValueError("'data' contains items which are lists of different length. All lists must be of equal length")
+            res = []
+            for i in range(n):
+                cpy = subs.copy()
+                for key in list_keys:
+                    cpy[key] = subs[key][i]
+                try:
+                    res.append(float(self._function.evalf(subs=cpy)))
+                except:
+                    res.append(self._function.evalf(subs=cpy))
+            return res
+        else:
+            try:
+                return float(self._function.evalf(subs=subs))
+            except:
+                return self._function.evalf(subs=subs)
+            
     
-    def evaluate_error_function(self, subs: dict, as_float: bool = False):
+    def evaluate_error_function(self, subs: dict = {}):
         """
         Evaluates the error function with the given values, which must be provided in the form of a Dictionary, for instance:
 
@@ -107,13 +129,33 @@ class Helper:
 
         evaluate_function({"A": 0.5, "C": 3}) This will leave "\\Delta A" and "\\Delta B" as variables
         """
-        if self.error_function == None:
-            self.calculate_error_function()
-        
-        if not all(item in self.consts_text + self.vars_text + self.errors_text for item in subs.keys()):
-            raise ValueError("The given variables and/or constants are not the same as in the error function")
-        return float(self.error_function.evalf(subs=subs)) if as_float else self.error_function.evalf(subs=subs)
-    
+        if subs:
+            if not all(item in self._consts_text + self._vars_text + self._errors_text for item in subs.keys()):
+                raise ValueError("The given variables and/or constants are not the same as in the error function")
+            return float(self._error_function.evalf(subs=subs))
+
+        subs = {**self.data_variables, **self.data_errors} 
+        subs = {e[0]: e[1] for e in subs.items() if e[1] != None}
+        if any(isinstance(e, list) for e in subs.values()):
+            list_keys = [e[0] for e in list(subs.items()) if isinstance(e[1], list)]
+            n = len(subs[list_keys[0]])
+            if not all(n == e for e in [len(subs[e]) for e in list_keys]):
+                raise ValueError("'data' contains items which are lists of different length. All lists must be of equal length")
+            res = []
+            for i in range(n):
+                cpy = subs.copy()
+                for key in list_keys:
+                    cpy[key] = subs[key][i]
+                try:
+                    res.append(float(self._error_function.evalf(subs=cpy)))
+                except:
+                    res.append(self._error_function.evalf(subs=cpy))
+            return res
+        else:
+            try:
+                return float(self._error_function.evalf(subs=subs))
+            except:
+                return self._error_function.evalf(subs=subs)
 
     def solve_function_for_variable(self, variable_to_solve: str, function_value = None, rest_of_variables: dict = None, symbolically = False):
         """
@@ -125,16 +167,20 @@ class Helper:
         error must take in order to match the experimental value
         """
         if symbolically:
-            function_value = sp.Symbol("f")
-            if variable_to_solve not in self.vars_text + self.consts_text:
-                raise ValueError("value of variable_to_solve is not a parameter of error function")
-            return sp.solve(sp.Equality(self.function, function_value), sp.Symbol(variable_to_solve))
+            function_value = Symbol("f")
+            if variable_to_solve not in self._vars_text + self._consts_text:
+                raise ValueError("value of variable_to_solve is not a parameter of error _function")
+            return solve(Equality(self._function, function_value), Symbol(variable_to_solve))
         else:
             if rest_of_variables == None:
-                raise ValueError("must provide rest_of_variables parameter, or set symbolically to True")
-            if not all(item in self.consts_text + self.vars_text + self.errors_text for item in rest_of_variables.keys()):
-                raise ValueError("The given variables and/or constants are not the same as in the error function")
-            return sp.solve(sp.Equality(self.function.evalf(subs=rest_of_variables), function_value), sp.Symbol(variable_to_solve))
+                if any(isinstance(e, list) for e in self.data_variables.values()):
+                    raise ValueError("Some variable in data_variables is a list. Cannot work with lists")
+                subs = {e[0]: e[1] for e in self.data_variables.items() if e[1] != None}
+                subs.pop(variable_to_solve)
+                return solve(Equality(self._function.evalf(subs=subs), function_value), Symbol(variable_to_solve))
+            if not all(item in self._consts_text + self._vars_text + self._errors_text for item in rest_of_variables.keys()):
+                raise ValueError("The given variables and/or constants are not the same as in the error _function")
+            return solve(Equality(self._function.evalf(subs=rest_of_variables), function_value), Symbol(variable_to_solve))
 
     def solve_error_function_for_variable(self, variable_to_solve: str, function_value = None, rest_of_variables: dict = None, symbolically = False):
         """
@@ -146,16 +192,21 @@ class Helper:
         error must take in order to match the experimental value
         """
         if symbolically:
-            function_value = sp.Symbol(self.error_mark + "f")
-            if variable_to_solve not in self.vars_text + self.consts_text + self.errors_text:
-                raise ValueError("value of variable_to_solve is not a parameter of error function")
-            return sp.solve(sp.Equality(self.error_function, function_value), sp.Symbol(variable_to_solve))
+            function_value = Symbol(self._error_mark + "f")
+            if variable_to_solve not in self._vars_text + self._consts_text + self._errors_text:
+                raise ValueError("value of variable_to_solve is not a parameter of error _function")
+            return solve(Equality(self._error_function, function_value), Symbol(variable_to_solve))
         else:
             if rest_of_variables == None:
-                raise ValueError("must provide rest_of_variables parameter, or set symbolically to True")
-            if not all(item in self.consts_text + self.vars_text + self.errors_text for item in rest_of_variables.keys()):
-                raise ValueError("The given variables and/or constants are not the same as in the error function")
-            return sp.solve(sp.Equality(self.error_function.evalf(subs=rest_of_variables), function_value), sp.Symbol(variable_to_solve))
+                subs = {**self.data_variables, **self.data_errors} 
+                subs = {e[0]: e[1] for e in subs.items() if e[1] != None}
+                subs.pop(variable_to_solve)
+                if any(isinstance(e, list) for e in subs.values()):
+                    raise ValueError("Some variable in data_variables is a list. Cannot work with lists")
+                return solve(Equality(self._error_function.evalf(subs=subs), function_value), Symbol(variable_to_solve))
+            if not all(item in self._consts_text + self._vars_text + self._errors_text for item in rest_of_variables.keys()):
+                raise ValueError("The given variables and/or constants are not the same as in the error _function")
+            return solve(Equality(self._error_function.evalf(subs=rest_of_variables), function_value), Symbol(variable_to_solve))
         
     def solve_error_function_for_all_variables(self, function_value = None, rest_of_variables: dict = None, symbolically: bool = False):
         """
@@ -165,35 +216,43 @@ class Helper:
         or the expression to get said result if "symbolically" is set to True
         """
         if symbolically:
-            function_value = sp.Symbol(self.error_mark + "f")
-            for error in self.errors:
+            function_value = Symbol(self._error_mark + "f")
+            for error in self._errors:
                 print("---------------")
                 display(error)
                 print("=\n")
-                display(sp.solve(sp.Equality(self.error_function, function_value), error))
+                display(solve(Equality(self._error_function, function_value), error))
                 print("---------------")
         else:
-            if rest_of_variables == None or function_value == None:
-                raise ValueError("must provide rest_of_variables and function_value parameters")
-            if not all(item in self.consts_text + self.vars_text + self.errors_text for item in rest_of_variables.keys()):
+            if rest_of_variables == None:
+                subs = {**self.data_variables, **self.data_errors} 
+                subs = {e[0]: e[1] for e in subs.items() if e[1] != None}
+                dict_cpy = subs.copy()
+                dict_cpy.pop(var.name)
+                print("---------------")
+                display(var)
+                print("=\n")
+                display(solve(Equality(self._error_function.evalf(subs=dict_cpy), function_value), var))
+                print("---------------")
+            if not all(item in self._consts_text + self._vars_text + self._errors_text for item in rest_of_variables.keys()):
                 raise ValueError("The given variables and/or constants are not the same as in the error function")
-            for var in self.vars:
+            for var in self._vars:
                 dict_cpy = rest_of_variables.copy()
                 dict_cpy.pop(var.name)
                 print("---------------")
                 display(var)
                 print("=\n")
-                display(sp.solve(sp.Equality(self.error_function.evalf(subs=dict_cpy), function_value), var))
+                display(solve(Equality(self._error_function.evalf(subs=dict_cpy), function_value), var))
                 print("---------------")
 
 
 def error_function_from_sympy_expression(function, variables):
     error_function = 0
     errors = [r"\Delta " + e for e in variables]
-    variables = [sp.Symbol(e) for e in variables]
-    errors = [sp.Symbol(e) for e in errors]
+    variables = [Symbol(e) for e in variables]
+    errors = [Symbol(e) for e in errors]
     for i in range(len(variables)): 
-        a = sp.Mul(errors[i], sp.diff(function, variables[i]), evaluate= False)
-        b = sp.Pow(a, 2, evaluate=False)
-        error_function = sp.Add(error_function, b)
-    return sp.sqrt(error_function)
+        a = Mul(errors[i], diff(function, variables[i]), evaluate= False)
+        b = Pow(a, 2, evaluate=False)
+        error_function = Add(error_function, b)
+    return sqrt(error_function)
